fc617c95ccbc6025b0fc3b212ee7ccefe6a496da
David Delgado <daviddn91@gmail.com>
2016-01-31 17:32:08 +0100

    Mapa funcionando dentro del Fragment por fin!!!

--- app/src/main/java/com/example/david/centroestudios/fragments/FragmentCercanas.java
+++ app/src/main/java/com/example/david/centroestudios/fragments/FragmentCercanas.java
@@ -36,6 +36,10 @@ public class FragmentCercanas extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    MapView mMapView;
+    private GoogleMap googleMap;
+
+
     private OnFragmentInteractionListener mListener;
 
     /**
@@ -72,8 +76,45 @@ public class FragmentCercanas extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_cercanas, container,
+                false);
+        mMapView = (MapView) v.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume();// needed to get the map to display immediately
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        googleMap = mMapView.getMap();
+        // latitude and longitude
+        double latitude = 17.385044;
+        double longitude = 78.486671;
+
+        // create marker
+        MarkerOptions marker = new MarkerOptions().position(
+                new LatLng(latitude, longitude)).title("Hello Maps");
+
+        // Changing marker icon
+        marker.icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+
+        // adding marker
+        googleMap.addMarker(marker);
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(17.385044, 78.486671)).zoom(12).build();
+        googleMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+
+        // Perform any camera updates here
+        return v;
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cercanas, container, false);
+        //return inflater.inflate(R.layout.fragment_cercanas, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -104,4 +145,27 @@ public class FragmentCercanas extends Fragment {
         public void onFragmentInteraction(Uri uri);
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
 }
