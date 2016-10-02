8670ccfaf644f84f7404f2ab743612135613e49e
David Delgado <daviddn91@gmail.com>
2016-10-02 18:37:46 +0200

    Limpieza de base de datos creando un checksum y moviendo dirección de casa y trabajo arriba

--- app/src/main/java/com/example/david/centroestudios/MainActivity.java
+++ app/src/main/java/com/example/david/centroestudios/MainActivity.java
@@ -81,11 +81,19 @@ public class MainActivity extends AppCompatActivity
         /* Base de datos */
         db = openOrCreateDatabase("BaseDeDatos", Context.MODE_PRIVATE, null);
 
+        Cursor c = db.rawQuery("SELECT * FROM centros WHERE id = '1234567890'", null);
+        if(!c.moveToFirst())
+        {
+            db.execSQL("DROP TABLE perfil");
+            db.execSQL("DROP TABLE filtros");
+            db.execSQL("DROP TABLE centros");
+        }
+
         // Creamos la tabla filtros con las preferencias
         db.execSQL("CREATE TABLE IF NOT EXISTS filtros (nina INTEGER, nino INTEGER, publico INTEGER, concertado INTEGER, privado INTEGER, religioso INTEGER, laico INTEGER, castellano INTEGER, catalan INTEGER, ingles INTEGER, frances INTEGER, aleman INTEGER, infantil1 INTEGER, infantil2 INTEGER, primaria INTEGER, eso INTEGER, bachillerato INTEGER);");
 
         // Ahora miramos si no existe nada en la tabla e insertamos los valores por defecto
-        Cursor c = db.rawQuery("SELECT * FROM filtros", null);
+        c = db.rawQuery("SELECT * FROM filtros", null);
         if(!c.moveToFirst())
         {
             db.execSQL("INSERT INTO filtros (nina, nino, publico, concertado, privado, religioso, laico, castellano, catalan, ingles, frances, aleman, infantil1, infantil2, primaria, eso, bachillerato) VALUES (1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1);");
@@ -93,6 +101,8 @@ public class MainActivity extends AppCompatActivity
 
         // Creamos la tabla con la info de los colegios y borramos los que sean muy antiguos
         db.execSQL("CREATE TABLE IF NOT EXISTS centros (id VARCHAR(100), nombre VARCHAR(100), direccion VARCHAR(1000), codigopostal VARCHAR(10), telefono VARCHAR(15), localidad VARCHAR(100), infantil1 VARCHAR(1), infantil2 VARCHAR(1), primaria VARCHAR(1), eso VARCHAR(1), bachillerato VARCHAR(1), actualizado VARCHAR(100), latitud VARCHAR(1000), longitud VARCHAR(1000));");
+
+        db.execSQL("INSERT INTO centros (id) VALUES ('1234567890');");
 
         //db.execSQL("DELETE FROM centros WHERE actualizado < sysdate()-30;");
 
